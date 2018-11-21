e3a63611e68a37a255f7152a28a614d55735d9ba
Jason <jason@Jasons-MacBook-Pro.local>
2018-11-21 18:20:42 +0800

    new p5 version

--- p5.py
+++ p5.py
@@ -1,14 +1,2 @@
-def check_palindrome(s):
-	s = s.lower()
-	for i in range (len(s)):
-			if (s[i] != s[len(s) - 1 - i]):
-				return False
-	return True
+print("heha")
 
-input_str = input("Please input a palindrome: ")
-while True:
-	if not check_palindrome(input_str):
-		input_str = input("No, you must input a palindrome: ")
-	else:
-		print ("Welcome to the wonderland!")
-		break
